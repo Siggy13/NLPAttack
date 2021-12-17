e1b3523b5dee104eb22f4bc4efc37be94cba6ff0
Yvonne Zino <y_zino@coloradocollege.edu>
2021-12-16 17:41:00 -0700

    add viper attack

--- NLP/AdversarialTool/attackEmotionsModel.py
+++ NLP/AdversarialTool/attackEmotionsModel.py
@@ -14,7 +14,8 @@ AttackDict={
     'PWWS':OpenAttack.attackers.PWWSAttacker(),
     'Genetic':OpenAttack.attackers.GeneticAttacker(),
     'SememePSO':OpenAttack.attackers.PSOAttacker(),
-    'FD': OpenAttack.attackers.FDAttacker()
+    'FD': OpenAttack.attackers.FDAttacker(),
+    'Viper': OpenAttack.attackers.VIPERAttacker()
     }
 
 def getEmotionAttackOutput(text, attack):
